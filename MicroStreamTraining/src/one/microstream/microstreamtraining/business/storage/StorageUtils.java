a6995e922a9504e38c52b534bfa14242bcf2221d
ckuemmel <ckuemmel@192.168.86.72>
2019-04-30 14:50:10 +0200

    Extendet Storage Implementation

--- MicroStreamTraining/src/one/microstream/microstreamtraining/business/storage/StorageUtils.java
+++ MicroStreamTraining/src/one/microstream/microstreamtraining/business/storage/StorageUtils.java
@@ -6,9 +6,25 @@ import one.microstream.X;
 import one.microstream.microstreamtraining.entities.DataRoot;
 import one.microstream.reference.Reference;
 import one.microstream.storage.types.EmbeddedStorage;
+import one.microstream.storage.types.EmbeddedStorageFoundation;
 import one.microstream.storage.types.EmbeddedStorageManager;
+import one.microstream.storage.types.Storage;
+import one.microstream.storage.types.StorageConfiguration.Builder;
 
 public class StorageUtils {
+	
 	public static final Reference<DataRoot> ROOT = X.Reference(new DataRoot());
-	public static final EmbeddedStorageManager STORAGE = EmbeddedStorage.start(ROOT, new File("C:/MicroStreamTraining"));
+	public static final EmbeddedStorageManager STORAGE;
+	public static final EmbeddedStorageFoundation<?> FOUNDATION;
+
+	static {
+		final Builder<?> configurationBuilder = Storage.ConfigurationBuilder()
+				.setStorageFileProvider(Storage.FileProvider(new File("C:/MicroStreamTraining")))
+				.setBackupSetup(Storage.BackupSetup("C:/MicroStreamTraining/Backup"))
+				.setChannelCountProvider(Storage.ChannelCountProvider(4));
+
+		FOUNDATION = EmbeddedStorage.Foundation(configurationBuilder.createConfiguration());
+		STORAGE = FOUNDATION.createEmbeddedStorageManager(ROOT).start();
+
+	}
 }
